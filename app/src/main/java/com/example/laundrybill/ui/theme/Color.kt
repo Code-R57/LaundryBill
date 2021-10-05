8bac04482e204ae2ed1f12b04fb3a355284029d7
Code-R57 <codingr57@gmail.com>
2021-10-05 15:35:42 +0530

    Added Missing Parameters in MainActivity

--- app/src/main/java/com/example/laundrybill/ui/theme/Color.kt
+++ app/src/main/java/com/example/laundrybill/ui/theme/Color.kt
@@ -1,12 +1,10 @@
 package com.example.laundrybill.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 
 val BackgroundDark = Color(0xFF111010)
 val BackgroundLight = Color(0x9AD7F3FF)
